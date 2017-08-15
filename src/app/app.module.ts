40e1a3c30aa43d4848b32db21b6a28efeae66fad
jpralves10 <jpralves10@gmail.com>
2017-08-14 21:20:20 -0300

    Restante da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {RouterModule} from '@angular/router'
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule, PreloadAllModules } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import {ROUTES} from './app.routes'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    NotFoundComponent,
+    RestaurantsComponent,
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    MenuItemComponent,
+    ReviewsComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES)
+    BrowserAnimationsModule,
+    HttpModule,
+    SharedModule.forRoot(),
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
   providers: [
+    {provide: LOCALE_ID, useValue: 'pt-BR'}
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/order/order.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core'
+import {RouterModule, Routes} from '@angular/router'
+
+import {SharedModule} from '../shared/shared.module'
+
+import {OrderComponent} from './order.component'
+import {OrderItemsComponent} from './order-items/order-items.component'
+import {DeliveryCostsComponent} from './delivery-costs/delivery-costs.component'
+import {OrderSummaryComponent} from './order-summary/order-summary.component'
+
+const ROUTES: Routes = [
+  {path: '', component: OrderComponent},
+  {path: 'order-summary', component: OrderSummaryComponent}
+]
+
+@NgModule({
+  declarations: [
+    OrderComponent,
+    OrderItemsComponent,
+    DeliveryCostsComponent,
+    OrderSummaryComponent
+  ],
+  imports: [
+    SharedModule,
+    RouterModule.forChild(ROUTES)
+  ]
+})
+export class OrderModule {}
